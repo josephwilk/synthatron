8fa954341f1b2c31c57c17a1f661775caff3db6a
Joseph Wilk <joe@josephwilk.net>
2016-08-24 12:21:57 +0100

    more permutations

--- src/synthatron/core.clj
+++ src/synthatron/core.clj
@@ -2,23 +2,50 @@
   (:use [overtone.live]))
 
 (def waves [sin-osc
-            lf-saw
-            lf-cub
-            lf-par
             pulse
             square
-            lf-tri])
+            ;;ouch  blip
+            saw
+
+            lf-saw
+            lf-tri
+            lf-par
+            lf-cub
+            lf-tri
+            lf-gauss
+            lf-pulse])
+
+(def envs [perc])
+
+(def effect [g-verb
+             pitch-shift
+             comb-n
+             pluck ])
+
+(def delay-ugens
+  [delay1 delay-n delay-l delay-c comb-n comb-l
+   comb-c allpass-n allpass-l allpass-c])
 
 (defn synth-builder []
   (let [n :blah
-        wave (choose waves)]
+        wave-count (+ 1 (rand-int (dec (count waves))))
+        waves (repeatedly wave-count #(choose waves))
+        envelope (choose envs)
+        effect (choose delay-ugens)
+
+        attack (rand)
+        release (rand)]
+    (println (map :name waves))
     (do
       (definst n [note 40]
         (let [freq (midicps note)
-              src (wave freq)
-              env (env-gen (env-perc))]
+              all-waves (map (fn [w] (w freq)) waves)
+              src (mix all-waves)
+              src (effect src)
+              env (env-gen (envelope attack release) :action FREE)]
           (* src env))))))
 
 (do
   (def new-synth (synth-builder))
   (new-synth :note (note :F#3)))
+;;(kill n)
